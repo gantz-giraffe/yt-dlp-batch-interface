11f68929be9cf28dd3844cedd53431c45e14f6b8
Noah <33939972+gantz-giraffe@users.noreply.github.com>
2024-04-20 17:44:59 -0500

    Update yt-dlp-interface.bat

--- yt-dlp-interface.bat
+++ yt-dlp-interface.bat
@@ -2,4 +2,4 @@
 
 echo Hi!
 set /p Input=Paste a url, then press enter:
-start /w yt-dlp.exe %Input%
+start /w yt-dlp.exe -f bv[ext=mp4]+ba[ext=m4a]/b --no-check-certificate --write-sub --sub-lang "en.*" %Input%
